4186611988352cff2866848a2990e04ce68c97bb
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-28 14:51:09 +0100

    Add functionality and a log message

--- extension.js
+++ extension.js
@@ -6,16 +6,30 @@ var vscode = require("vscode");
 // your extension is activated the very first time the command is executed
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
-  //console.log("Congratulations, your extension 'extended-cursormove' is now active!");
+  console.log("Congratulations, your extension 'smartHome' is now active!");
 
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand("extension.smartHome.toggleCursorHome", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "wrappedLineStart",
-        by: "line",
-        select: false,
-        value: 1
-      });
+      if (context.toggledOn) {
+        // Go the the start of the text
+        activeEditor.selection.active.line;
+        vscode.commands.executeCommand("cursorMove", {
+          to: "wrappedLineFirstNonWhitespaceCharacter",
+          by: "line",
+          select: false,
+          value: 1
+        });
+        context.toggledOn = false;
+      } else {
+        // Go to the very start of the line
+        vscode.commands.executeCommand("cursorMove", {
+          to: "wrappedLineStart",
+          by: "line",
+          select: false,
+          value: 1
+        });
+        context.toggledOn = true;
+      }
     })
   );
 
