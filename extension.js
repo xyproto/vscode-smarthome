3dcdf95ac375842f501e75e8fdbffeee7ac5e306
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-28 15:09:12 +0100

    Make it work

--- extension.js
+++ extension.js
@@ -2,24 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require("vscode");
 
+var toggledOn = false;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
-  console.log("Congratulations, your extension 'smartHome' is now active!");
+  //console.log("Congratulations, your extension 'smartHome' is now active!");
 
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand("extension.smartHome.toggleCursorHome", function(editor, edit) {
-      if (context.toggledOn) {
+      if (toggledOn == false) {
         // Go the the start of the text
-        activeEditor.selection.active.line;
+        //activeEditor.selection.active.line;
         vscode.commands.executeCommand("cursorMove", {
           to: "wrappedLineFirstNonWhitespaceCharacter",
           by: "line",
           select: false,
           value: 1
         });
-        context.toggledOn = false;
+        toggledOn = true;
       } else {
         // Go to the very start of the line
         vscode.commands.executeCommand("cursorMove", {
@@ -28,7 +30,7 @@ function activate(context) {
           select: false,
           value: 1
         });
-        context.toggledOn = true;
+        toggledOn = false;
       }
     })
   );
