bf5a04dbad288b4ed872507d04020cfe23b2ac24
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-28 14:44:14 +0100

    Initial commit

--- extension.js
+++ extension.js
@@ -9,117 +9,13 @@ function activate(context) {
   //console.log("Congratulations, your extension 'extended-cursormove' is now active!");
 
   context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorHome", function(editor, edit) {
+    vscode.commands.registerTextEditorCommand("extension.smartHome.toggleCursorHome", function(editor, edit) {
       vscode.commands.executeCommand("cursorMove", {
         to: "wrappedLineStart",
         by: "line",
         select: false,
         value: 1
       });
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorHomeSelect", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "wrappedLineStart",
-        by: "line",
-        select: true,
-        value: 1
-      });
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorEnd", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "wrappedLineEnd",
-        by: "line",
-        select: false,
-        value: 1
-      });
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorEndSelect", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "wrappedLineEnd",
-        by: "line",
-        select: true,
-        value: 1
-      });
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorTopLeft", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "viewPortTop",
-        by: "line",
-        select: false,
-        value: 1
-      });
-      vscode.commands.executeCommand("extension.extendedCursorMove.cursorHome");
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorTopLeftSelect", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "viewPortTop",
-        by: "line",
-        select: true,
-        value: 1
-      });
-      vscode.commands.executeCommand("extension.extendedCursorMove.cursorHomeSelect");
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorBottomRight", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "viewPortBottom",
-        by: "line",
-        select: false,
-        value: 1
-      });
-      vscode.commands.executeCommand("extension.extendedCursorMove.cursorEnd");
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.cursorBottomRightSelect", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "viewPortBottom",
-        by: "line",
-        select: true,
-        value: 1
-      });
-      vscode.commands.executeCommand("extension.extendedCursorMove.cursorEndSelect");
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.centerCursorInView", function(editor, edit) {
-      vscode.commands.executeCommand("revealLine", {
-        lineNumber: editor.selection.anchor.line,
-        at: "center"
-      });
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand("extension.extendedCursorMove.moveCursorToView", function(editor, edit) {
-      vscode.commands.executeCommand("cursorMove", {
-        to: "viewPortCenter",
-        by: "line",
-        select: ! editor.selection.isEmpty,
-        value: 1
-      });
-      editor.selection.isEmpty ?
-        vscode.commands.executeCommand("extension.extendedCursorMove.cursorHome") :
-        vscode.commands.executeCommand("extension.extendedCursorMove.cursorHomeSelect");
     })
   );
 
